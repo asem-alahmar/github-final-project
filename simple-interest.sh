98d873707599b543a0b2ed436436483e75426ff9
Mohamad Asem Alahmar <73745574+asem-alahmar@users.noreply.github.com>
2023-02-27 01:30:49 +0300

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
    # Do not use this in production. Sample purpose only.
 
-   # Author: Upkar Lidder (IBM)
+   # Author: Asem-alahmar (IBM)
    # Additional Authors:
-   # <your GitHub username>
+   # asem-alahmar
 
    # Input:
    # p, principal amount
